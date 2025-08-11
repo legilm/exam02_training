673b044d20d1fa87c4175fe3366fe5e1f85a62d3
Gil <gluz-ste@student.42luxembourg.lu>
2025-08-11 13:40:24 +0200

    do_op

---
+++ do_op/do_op.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	long	first_nbr;
+	long	second_nbr;
+	char	operator;
+	long	result;
+
+	first_nbr = 0;
+	if (ac != 4)
+		return (printf ("\n"));
+	first_nbr = atoi(av[1]);
+	second_nbr = atoi(av[3]);
+	operator = av[2][0];
+	if (operator != '+' && operator != '-' && operator != '*' && operator != '/')
+		return (printf ("\n"));
+	if (operator == '+')
+		result = first_nbr + second_nbr;
+	if (operator == '-')
+		result = first_nbr - second_nbr;
+	if (operator == '*')
+		result = first_nbr * second_nbr;
+	if (operator == '/')
+		result = first_nbr / second_nbr;
+	return (printf("%ld", result));
+}
