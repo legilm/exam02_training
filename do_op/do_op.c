37079ccb122b8f394acc7e2f85bc40a4a3bdc160
legilm <107485548+legilm@users.noreply.github.com>
2025-08-12 05:54:35 +0200

    ft_strcmp

--- do_op/do_op.c
+++ do_op/do_op.c
@@ -8,14 +8,15 @@ int	main(int ac, char **av)
 	char	operator;
 	long	result;
 
-	first_nbr = 0;
 	if (ac != 4)
 		return (printf ("\n"));
 	first_nbr = atoi(av[1]);
 	second_nbr = atoi(av[3]);
 	operator = av[2][0];
-	if (operator != '+' && operator != '-' && operator != '*' && operator != '/')
+	if (operator != '+' && operator != '-' && operator != '*' && operator != '/' && operator != '%')
 		return (printf ("\n"));
+	if ((operator == '/' || operator == '%') && second_nbr == 0)
+		return (printf("Error\n"));
 	if (operator == '+')
 		result = first_nbr + second_nbr;
 	if (operator == '-')
@@ -26,5 +27,5 @@ int	main(int ac, char **av)
 		result = first_nbr / second_nbr;
 	if (operator == '%')
 		result = first_nbr % second_nbr;
-	return (printf("%ld", result));
+	return (printf("%ld\n", result));
 }
