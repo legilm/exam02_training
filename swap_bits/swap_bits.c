5c5c7b032356141614e664957bc32daca48724f7
Gil <gluz-ste@student.42luxembourg.lu>
2025-08-13 10:35:32 +0200

    correction

--- swap_bits/swap_bits.c
+++ swap_bits/swap_bits.c
@@ -1,4 +1,24 @@
 unsigned char	swap_bits(unsigned char octet)
 {
-	return ((octet >> 4) || (octet << 4));
+	return ((octet >> 4) | (octet << 4));
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+	unsigned char test_values[] = {0x12, 0xAB, 0x00, 0xFF, 0x41};
+	unsigned char swapped;
+	int i = 0;
+
+	// Test multiple values in the array
+	while (i < 5)
+	{
+		swapped = swap_bits(test_values[i]);
+
+		printf("Original: 0x%X, Swapped: 0x%X\n", test_values[i], swapped);
+		i++;
+	}
+
+	return 0;
 }
